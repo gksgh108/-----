f7b266bb93802a8c5286199b7e4a0b2ce80b1f8d
김한호 <gksgh108@gmail.com>
2023-04-14 12:41:53 +0900

    헉

---
+++ SWFramework/src/main/java/com/example/demo/SwFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwFrameworkApplication.class, args);
+	}
+
+}
